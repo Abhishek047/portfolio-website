7816937b6d57e564e9b17ea293f51a61b98d477f
Abhishek Shukla <abhi.shukla0047@gmail.com>
2020-10-21 05:20:30 -0700

    Updates

---
+++ precache-manifest.16017c0cfe3e8858ad02979afaf48826.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae72114c9b829bf7e75ab3eb9b5e6b30",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "06545cc0641ba6c651db",
+    "url": "/portfolio-website/static/css/main.fa126258.chunk.css"
+  },
+  {
+    "revision": "00a5bf47a83f9d5b8f82",
+    "url": "/portfolio-website/static/js/2.5f85ceb7.chunk.js"
+  },
+  {
+    "revision": "53db32a7128edc3af4dcda73b74cdd83",
+    "url": "/portfolio-website/static/js/2.5f85ceb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06545cc0641ba6c651db",
+    "url": "/portfolio-website/static/js/main.41b9727e.chunk.js"
+  },
+  {
+    "revision": "50010564107c13acc088",
+    "url": "/portfolio-website/static/js/runtime-main.4c4711bc.js"
+  },
+  {
+    "revision": "c65c9f3b55a1d25a0ba932820270c8f2",
+    "url": "/portfolio-website/static/media/Facebook.c65c9f3b.svg"
+  },
+  {
+    "revision": "5435196eb1af6ed5b988c3d0c5616e39",
+    "url": "/portfolio-website/static/media/Ghost.5435196e.svg"
+  },
+  {
+    "revision": "165255e6a32a9a5d1fd1fa4f6ed5eece",
+    "url": "/portfolio-website/static/media/Git.165255e6.svg"
+  },
+  {
+    "revision": "c2560d0548a973bb4b57fac3bdb47845",
+    "url": "/portfolio-website/static/media/LinkedIn.c2560d05.svg"
+  },
+  {
+    "revision": "3dde40499174752156176f58fff3b84b",
+    "url": "/portfolio-website/static/media/Profile.3dde4049.jpg"
+  },
+  {
+    "revision": "d0eec035b6cbfd9f2d39659d5263f6fc",
+    "url": "/portfolio-website/static/media/ProjectBg.d0eec035.svg"
+  },
+  {
+    "revision": "fbf1d7ae14fc2dadfdf01af6ae0b7091",
+    "url": "/portfolio-website/static/media/RightArrow.fbf1d7ae.svg"
+  },
+  {
+    "revision": "42488afa2489c5ba8d74841741f838aa",
+    "url": "/portfolio-website/static/media/WhatsApp.42488afa.svg"
+  },
+  {
+    "revision": "a0b883b8e7419dfd3d31f6635d8f0fcc",
+    "url": "/portfolio-website/static/media/dots.a0b883b8.svg"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/portfolio-website/static/media/exp_icon.98ff4822.png"
+  },
+  {
+    "revision": "509191fad4a92ec6e3c5c3628e41ed36",
+    "url": "/portfolio-website/static/media/img.509191fa.jpg"
+  },
+  {
+    "revision": "60605a1a458a3de31e7e4b5cc3605d13",
+    "url": "/portfolio-website/static/media/mongo_icon.60605a1a.png"
+  },
+  {
+    "revision": "018a11afda043f365be8b7e9a32fb245",
+    "url": "/portfolio-website/static/media/node_icon.018a11af.png"
+  },
+  {
+    "revision": "c7fb02ca5aa39b2c5da4fc009d858fc1",
+    "url": "/portfolio-website/static/media/r_icon.c7fb02ca.png"
+  }
+]);
